0db7ba211bc20e1c85658e4459d00c5ab3e2a4c5
PaulsKK <129509279+PaulsKK@users.noreply.github.com>
2024-01-24 15:15:47 +0200

    Add files via upload

---
+++ BookController.php
@@ -0,0 +1,17 @@
+<?php
+// app/Http/Controllers/BookController.php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        $books = Book::all();
+
+        return view('books.index', compact('books'));
+    }
+}
