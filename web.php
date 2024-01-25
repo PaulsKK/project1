0f3e7bda12d054375a53e27da61b0389e0358c7d
PaulsKK <129509279+PaulsKK@users.noreply.github.com>
2024-01-25 10:25:54 +0200

    \web.php

--- web.php
+++ web.php
@@ -2,6 +2,9 @@
 // routes/web.php
 
 use App\Http\Controllers\CustomPageController;
- 
+use App\Http\Controllers\YourController;
+
 Route::get('/', [CustomPageController::class, 'index']);
+Route::get('/show-data', [YourController::class, 'showData']);
+
 
